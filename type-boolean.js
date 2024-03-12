359aaa55e4a87e17f8ea21416d80889292f6983c
anajulia2112 <159189068+anajulia2112@users.noreply.github.com>
2024-03-12 14:35:46 -0300

    Add files via upload

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+//boolean
+//true -> falso
+//false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+const casdastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura"
+const texto2 = "Alura"
+console.log(texto1 === texto2)
